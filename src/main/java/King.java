75c244a7bce9bede083c5290938be561a8aa0d11
Jo Shin <44675895+j-os-h-in@users.noreply.github.com>
2025-08-15 00:28:46 +0800

    Add support for list

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class King {
@@ -8,12 +9,24 @@ public class King {
         System.out.println("    ____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         String text = scanner.nextLine();
 
         while (!text.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + text.toUpperCase() + "!!");
-            System.out.println("    ____________________________________________________________");
+            if (text.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println("     " + i + ". " + list.get(i - 1));
+                }
+                System.out.println("    ____________________________________________________________");
+
+            }
+            else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " + text.toUpperCase() + "!!");
+                System.out.println("    ____________________________________________________________");
+                list.add(text.toUpperCase() + "!!");
+            }
             text = scanner.nextLine();
         }
 
