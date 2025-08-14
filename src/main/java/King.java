c01f52a3935bd7f9852b348d53884eb0e699e22c
Jo Shin <44675895+j-os-h-in@users.noreply.github.com>
2025-08-14 23:58:50 +0800

    Renamed Duke to King and added greeting for bot

---
+++ src/main/java/King.java
@@ -0,0 +1,9 @@
+public class King {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm King!");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
