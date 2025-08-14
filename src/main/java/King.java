10254249e46f5887719061613197836a4a545945
Jo Shin <44675895+j-os-h-in@users.noreply.github.com>
2025-08-15 01:35:31 +0800

    Add support for completable task

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -9,30 +9,45 @@ public class King {
         System.out.println("    ____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         String text = scanner.nextLine();
 
         while (!text.equals("bye")) {
-            if (text.equals("list")) {
-                System.out.println("    ____________________________________________________________");
-                for (int i = 1; i <= list.size(); i++) {
-                    System.out.println("     " + i + ". " + list.get(i - 1));
+            System.out.println("    ____________________________________________________________");
+            try {
+                if (text.equals("list")) {
+                    System.out.println("     Here are the tasks in your list:");
+                    for (int i = 1; i <= list.size(); i++) {
+                        System.out.println("     " + i + ". " + list.get(i - 1));
+                    }
                 }
-                System.out.println("    ____________________________________________________________");
-
+                else if (text.strip().matches("mark [0-9]+")){
+                    int idx = Integer.parseInt(text.strip().substring(5)) - 1;
+                    list.get(idx).markDone();
+                    System.out.println("     NICE!! I've marked this task as done:");
+                    System.out.println("       " + list.get(idx));
+                }
+                else if (text.strip().matches("unmark [0-9]+")){
+                    int idx = Integer.parseInt(text.strip().substring(7)) - 1;
+                    list.get(idx).unmarkDone();
+                    System.out.println("     OK :( I've marked this task as not done yet");
+                    System.out.println("       " + list.get(idx));
+                }
+                else {
+                    System.out.println("     added: " + text.toUpperCase() + "!!");
+                    list.add(new Task(text.toUpperCase() + "!!"));
+                }
             }
-            else {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     added: " + text.toUpperCase() + "!!");
-                System.out.println("    ____________________________________________________________");
-                list.add(text.toUpperCase() + "!!");
+            catch (IndexOutOfBoundsException e) {
+                System.out.println("     Error! Task does not exist... ");
             }
+            System.out.println("    ____________________________________________________________");
             text = scanner.nextLine();
         }
 
         scanner.close();
         System.out.println("    ____________________________________________________________");
-        System.out.println("     BYEBYE!! Hope to see you again soon!");
+        System.out.println("     BYE BYE!! Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
 }
