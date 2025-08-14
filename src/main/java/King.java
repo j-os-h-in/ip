9def82c3b700e92610d5d1f261a0571028219b0a
Jo Shin <44675895+j-os-h-in@users.noreply.github.com>
2025-08-15 00:19:46 +0800

    Add support for echo and customised reply text

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class King {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm King!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm King!");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        while (!text.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + text.toUpperCase() + "!!");
+            System.out.println("    ____________________________________________________________");
+            text = scanner.nextLine();
+        }
+
+        scanner.close();
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     BYEBYE!! Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
