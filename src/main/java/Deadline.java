283510b5ccb7af69c58b417d9dcc18882b770491
Jo Shin <44675895+j-os-h-in@users.noreply.github.com>
2025-08-24 15:54:29 +0800

    Added proper datetime formatting, added proper comments

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,23 +1,45 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) throws KingException {
+public class Deadline extends Task {
+    private LocalDate by;
+
+    /**
+     * Instantiates a deadline task based on the description and deadline of the task.
+     * If no deadline is provided, throws a missing deadline exception.
+     * @param description Description of the task.
+     * @param by Deadline of the task.
+     * @throws KingException Error in creation of task.
+     */
+    public Deadline(String description, LocalDate by) throws KingException {
         super(description);
         if (by == null) { throw new KingException(KingException.ErrorMessage.DEADLINE_MISSING_DEADLINE); }
         else {this.by = by;}
     }
 
-    public String getBy() {
+    /**
+     * Returns the completion date of the task.
+     * @return Date of completion.
+     */
+    public LocalDate getBy() {
         return this.by;
     }
 
+    /**
+     * Sets the deadline of the task.
+     * @param by Task deadline.
+     */
+    public void setBy(LocalDate by) {
+        this.by = by;
+    }
+
     @Override
-    public String getType() {
-        return "DEADLINE";
+    public Type getType() {
+        return Type.DEADLINE;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ")";
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,35 +1,84 @@
 public abstract class Task {
-    protected String description;
-    protected boolean isDone;
+    private String description;
+    private boolean complete;
+    public enum Type {
+        TODO,
+        DEADLINE,
+        EVENT
+    }
 
+    /**
+     * Instantiates a task based on the description.
+     * If no description is provided, throws a missing description exception.
+     * @param description Description of the task.
+     * @throws KingException Error in creation of task.
+     */
     public Task(String description) throws KingException {
         if (description == null) {
             throw new KingException(KingException.ErrorMessage.MISSING_TASK_DESCRIPTION);
         }
         else {
             this.description = description;
-            this.isDone = false;
+            this.complete = false;
         }
     }
 
-    public abstract String getType();
+    /**
+     * Returns the type of the task.
+     * @return Task type.
+     */
+    public abstract Type getType();
 
-    public String getStatusIcon() {
-        return (isDone ? "X" : " ");
-    }
-
+    /**
+     * Returns the description of the task.
+     * @return Description of task.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Returns the completion status of the task.
+     * @return If task is complete return true, else false.
+     */
+    public boolean getComplete() {
+        return this.complete;
+    }
+
+    /**
+     * Returns the completion status icon "X" of the task.
+     * @return If task is complete return "X", else " ".
+     */
+    public String getStatusIcon() {
+        return (complete ? "X" : " ");
+    }
+
+    /**
+     * Sets the description of the task.
+     * @param description Description of task.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Sets the task to complete.
+     */
     public void markDone() {
-        this.isDone = true;
+        this.complete = true;
     }
 
+    /**
+     * Sets the task to incomplete.
+     */
     public void unmarkDone() {
-        this.isDone = false;
+        this.complete = false;
     }
 
+    /**
+     * Returns the string representation of the task.
+     * @return String representation of task.
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
